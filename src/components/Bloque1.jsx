a1361088eae869889431b5985da50706c9b5c02e
rojasbarriosalexander <rojasbarriosalexander@gmail.com>
2024-04-01 16:38:20 -0500

    ss2

--- src/components/Bloque1.jsx
+++ src/components/Bloque1.jsx
@@ -3,13 +3,11 @@ import { VentasNuevo } from './VentasNuevo';
 
 export const Bloque1 = () => {
   return (
-    <div className=' bg-white shadow-container ring-1 ring-gray-900/5 sm:mx-auto rounded-2xl my-7 pt-3 w-563'>
-        <div className='flex flex-col gap-4'>
-        <header className='px-3'>
-            <strong className='text-xl text-purple-dark'>Finanzas</strong>
-        </header>
-        <VentasNuevo/>
-        </div>
+    <div className=' bg-white shadow-container ring-1 flex flex-col gap-4 ring-gray-900/5 sm:mx-auto rounded-2xl my-7 pt-3 w-563'>
+      <header className='px-3'>
+          <strong className='text-xl text-purple-dark'>Finanzas</strong>
+      </header>
+      <VentasNuevo/>
     </div>
   )
 }
--- src/components/Bloque3.jsx
+++ src/components/Bloque3.jsx
@@ -4,17 +4,15 @@ import { VentasNuevo } from './VentasNuevo';
 
 export const Bloque3 = () => {
   return (
-    <div className=" bg-white shadow-container ring-1 ring-gray-900/5 sm:mx-auto rounded-2xl my-7 pt-3 w-563">
-        <div className='flex flex-col gap-4'>
-            <header className='px-3'>
-                <strong className='text-xl text-purple-dark'>Finanzas</strong>
-            </header>
-            <ul className=''>
-                <VentasNuevo/>
-                <Ventas/>
-                <VentasNuevo/>
-            </ul>
-        </div>
+    <div className=" bg-white shadow-container flex flex-col gap-4 ring-1 ring-gray-900/5 sm:mx-auto rounded-2xl my-7 pt-3 w-563">
+        <header className='px-3'>
+            <strong className='text-xl text-purple-dark'>Finanzas</strong>
+        </header>
+        <ul className=''>
+            <VentasNuevo/>
+            <Ventas/>
+            <VentasNuevo/>
+        </ul>
     </div>
   )
 }
